ddac01ad5adfef176ff870dd503a55568ed00a3c
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-10-05 21:00:27 +0800

    update context grant method

--- app/src/main/java/org/baiyu/fuckssaid/MainHook.java
+++ app/src/main/java/org/baiyu/fuckssaid/MainHook.java
@@ -1,6 +1,6 @@
 package org.baiyu.fuckssaid;
 
-import android.app.Application;
+import android.app.AndroidAppHelper;
 import android.content.Context;
 import android.provider.Settings;
 
@@ -9,12 +9,9 @@ import java.util.UUID;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
-
-    private Context context;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
@@ -22,17 +19,6 @@ public class MainHook implements IXposedHookLoadPackage {
             return;
         }
         try {
-            XposedHelpers.findAndHookMethod(Application.class,
-                    "attach",
-                    Context.class,
-                    new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            super.afterHookedMethod(param);
-                            context = (Context) param.args[0];
-                        }
-                    });
-
             XposedBridge.hookAllMethods(
                     Settings.Secure.class,
                     "getString",
@@ -40,6 +26,7 @@ public class MainHook implements IXposedHookLoadPackage {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                             super.beforeHookedMethod(param);
+                            Context context = AndroidAppHelper.currentApplication();
                             if (context == null) {
                                 XposedBridge.log("Fuck SSAID: cannot get context for " + lpparam.packageName);
                                 return;
@@ -50,7 +37,7 @@ public class MainHook implements IXposedHookLoadPackage {
                             if (id == null) {
                                 id = UUID.randomUUID().toString();
                                 settings.setIdForPackage(id);
-                                XposedBridge.log("Fuck SSAID: new id set for " + lpparam.packageName + ": " + id);
+                                XposedBridge.log("Fuck SSAID: new id generated for " + lpparam.packageName + ": " + id);
                             }
 
                             if (param.args[1] instanceof String s && s.equals(Settings.Secure.ANDROID_ID)) {
